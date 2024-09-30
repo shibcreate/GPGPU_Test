186016675fb0f1acff4b27ff7ba00b71db61f779
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2024-09-30 10:54:44 -0700

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,32 +1,29 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
 
 #define N 1000
 
-
 __global__ void vector_add_cuda(float *out, float *a, float *b, int n) {
-    
     int id = blockDim.x * blockIdx.x + threadIdx.x;
     
-    if(id < N)
-    {
+    if (id < n) {
         out[id] = a[id] + b[id];
     }
 }
 
-int main(){
-
-    // Number of bytes to allocate for N doubles
+int main() {
+    // Number of bytes to allocate for N floats
     size_t bytes = N * sizeof(float);
 
-    float *h_a, *h_b, *out, *h_out, *d_a, *d_b, *d_out; 
+    float *h_a, *h_b, *h_out, *d_a, *d_b, *d_out;
 
-    // Allocate data in host pointer
+    // Allocate data in host pointers
     cudaMallocHost(&h_a, bytes);
     cudaMallocHost(&h_b, bytes);
     cudaMallocHost(&h_out, bytes);
 
-    // Initialize array
-    for(int i = 0; i < N; i++){
+    // Initialize arrays
+    for (int i = 0; i < N; i++) {
         h_a[i] = 1.0f;
         h_b[i] = 2.0f;        
     }
@@ -36,38 +33,30 @@ int main(){
     cudaMalloc(&d_b, bytes);
     cudaMalloc(&d_out, bytes);
 
-
     // Transfer data from host to device memory
     cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
-
     int THREADS = 256;
     int BLOCKS = (N + THREADS - 1) / THREADS;
-
 
     // Kernel function
     vector_add_cuda <<< BLOCKS, THREADS >>> (d_out, d_a, d_b, N);
 
-
     // Transfer computed data from device to host memory
     cudaMemcpy(h_out, d_out, bytes, cudaMemcpyDeviceToHost);
 
-
     // Check/validate results
-    for(int i = 0; i < N; i++)
-    {
-        if (i < 10)
-        {
-            printf("h_a[%d] = %.5lf, h_out[%d] = %.5lf\n", i, h_a[i], i, h_out[i]);
+    for (int i = 0; i < N; i++) {
+        if (i < 10) {
+            printf("h_a[%d] = %.5f, h_out[%d] = %.5f\n", i, h_a[i], i, h_out[i]);
         }
 
-        if (out[i] != h_out[i])
-        {
-            printf(" Error at %d, h_out[%d] = %.5lf, out[%d] = %.5lf\n", i, i, h_out[i], i, out[i]);
+        // Change 'out[i]' to expected result (1.0 + 2.0)
+        if (h_out[i] != 3.0f) {
+            printf("Error at %d, h_out[%d] = %.5f, expected = 3.0\n", i, i, h_out[i]);
             break;
         }
-
     }
 
     // Cleanup after kernel execution
@@ -75,9 +64,9 @@ int main(){
     cudaFree(d_b);
     cudaFree(d_out);
 
-    cudaFree(h_a);
-    cudaFree(h_b);
-    cudaFree(h_out);
+    cudaFreeHost(h_a);
+    cudaFreeHost(h_b);
+    cudaFreeHost(h_out);
 
     return 0;
 }
