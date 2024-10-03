1f7a6f40a365f6f19e94a9b24579de751e2065b1
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2024-10-03 15:05:45 -0700

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -1,5 +1,14 @@
 #include <stdio.h>
- 
-__global__ void VecAdd(const float *A, const float *B, float* C, int n) {
 
+__global__ void MatMul(const float* A, const float* B, float* C, int n) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < n && col < n) {
+        float value = 0;
+        for (int k = 0; k < n; k++) {
+            value += A[row * n + k] * B[k * n + col];
+        }
+        C[row * n + col] = value;
+    }
 }
